4b8863b5184b0d5cc7f88a36303a3d853a5b1953
许 <>
2019-07-16 10:45:00 +0800

    Initial Commit

---
+++ 多界面传值/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  多界面传值
+//
+//  Created by 许明洋 on 2019/7/16.
+//  Copyright © 2019 许明洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 多界面传值/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  多界面传值
+//
+//  Created by 许明洋 on 2019/7/16.
+//  Copyright © 2019 许明洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
