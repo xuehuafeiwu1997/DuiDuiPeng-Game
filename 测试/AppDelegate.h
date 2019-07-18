9830906961b28747cdaecb8edbb129bd8a413c3b
许明洋 <2500207996@qq.com>
2019-07-18 19:52:46 +0800

    Initial Commit

---
+++ 测试/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  测试
+//
+//  Created by 许明洋 on 2019/7/18.
+//  Copyright © 2019 许明洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 测试/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  测试
+//
+//  Created by 许明洋 on 2019/7/18.
+//  Copyright © 2019 许明洋. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
