0ba0f9e1fef19bad6e884a5b6442b3ab502100d6
xuehuafeiwu1997 <2500207996@qq.com>
2019-07-31 18:35:17 +0800

    对对碰游戏

---
+++ 对对碰游戏/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  对对碰游戏
+//
+//  Created by 许明洋 on 2019/7/19.
+//  Copyright © 2019 许明洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 对对碰游戏/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  对对碰游戏
+//
+//  Created by 许明洋 on 2019/7/19.
+//  Copyright © 2019 许明洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
